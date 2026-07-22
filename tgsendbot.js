e8b55ebf7a10330b17894a62575b577155fd95c8
shadow <usolimaxamatov10@gmail.com>
2026-07-22 16:51:59 +0500

    start

--- tgsendbot.js
+++ tgsendbot.js
@@ -1,12 +1,18 @@
 let token = "8990191602:AAFeFNg1gKOh2VI1N4dZIJwAvxLwVIa6Flg"
 let tgID = "6421571071"
 let nameInput = document.getElementById("ism");
-let phoneInput = document.getElementById("telefon");
+let phoneInput =+ document.getElementById("telefon");
 let addressInput = document.getElementById("manzil");
 let additionalInput = document.getElementById("qoshimcha-izoh");
 let submitButton = document.getElementById("submit");
 
-
+if(phoneInput.value.length < 9 || phoneInput.value.length > 13) {
+  alert("Telefon raqami 9 dan 13 gacha bo'lishi kerak");
+  return;
+}else if(isNaN(phoneIInput.value)) {
+  alert("Telefon raqami faqat raqamlar bo'lishi kerak");
+  return;
+}
 
 function sendTGBot() {
   fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
